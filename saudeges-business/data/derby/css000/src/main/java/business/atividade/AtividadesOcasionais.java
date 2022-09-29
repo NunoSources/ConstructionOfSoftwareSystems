10566f6693d672f3ae1d13db9f40efbcad2af911
NunoSources <98571427+NunoSources@users.noreply.github.com>
2022-09-29 14:40:42 +0100

    Delete saudeges-business/data/derby/css000/src/main directory

--- saudeges-business/data/derby/css000/src/main/java/business/atividade/AtividadesOcasionais.java
+++
@@ -1,9 +0,0 @@
-package business.atividade;
-
-public abstract class AtividadesOcasionais {
-
-	public static boolean nrSessoesValido(int nrSessoes) {
-		return nrSessoes >= 1 && nrSessoes <= 20;
-	}
-
-}
--- saudeges-business/data/derby/css000/src/main/java/business/atividade/AtividadesRegulares.java
+++
@@ -1,9 +0,0 @@
-package business.atividade;
-
-public abstract class AtividadesRegulares {
-
-	public static boolean nrSessoesValido(int nrSessoes) {
-		return nrSessoes >= 1 && nrSessoes <= 5;
-	}
-
-}
